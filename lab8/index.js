bed4b394095a5c5b76709da3ab72b2e4df1e0ab7
jaden416 <jaden416@outlook.com>
2023-03-24 14:59:23 -0400

    test

--- lab8/index.js
+++ lab8/index.js
@@ -1,7 +1,7 @@
 // Imports
 import express from "express"
 import fetch from "node-fetch"
-import posts from "./lab8posts_backUp.json" assert {type: 'json'}
+import bodyParser from "body-parser"
 
 // Variables
 const app = express()
@@ -13,7 +13,8 @@ app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}.`)
 })
 
-
+// use body-parser
+app.use(bodyParser.json())
 
 // Add your routes below:
 
@@ -36,15 +37,74 @@ app.get("/posts/author/:author", async (req, res) => {
 
   res.send(filteredPosts)
 })
+
 // get posts by postID
-app.get("/posts/author/:postID", async (req, res) => {
-  const author = req.params.postID
+app.get("/posts/:postID", async (req, res) => {
   const data = await fetch(basket)
   const newData = await data.json()
 
-  const filteredPosts = newData.posts.filter((posts) => posts.author === postID)
+  //finds the specific postID then shares the post
+  const getId = Number(req.params.postID)
 
-  res.send(filteredPosts)
+  let id = newData.posts.filter((post) => post.postId === getId)
+
+  res.send(...id)
 })
 
 // post a post
+app.post("/posts", async (req, res) => {
+
+  const data = await fetch(basket)
+  const newData = await data.json()
+
+  const item = req.body
+  console.log(item)
+  
+  newData.posts.push(item)
+  
+  let gData 
+ try {
+    gData = await fetch (basket, {
+      method: "POST",
+      body: JSON.stringify(newData),
+      headers: { "Content-Type": "applications/json"}
+    })
+ } catch (error){
+  console.log(error)
+ }
+
+  res.json(newData)
+})
+
+// delete posts by postID
+app.delete("/posts/:postID", async (req, res) => {
+  const data = await fetch(basket)
+  const newData = await data.json()
+
+  //finds the specific postID then shares the post
+  const getId = Number(req.params.postID)
+
+  let id = newData.posts.filter((post) => post.postId === getId)
+
+  let index = newData.posts.findIndex((post) => post.postId === getId)
+
+  newData.posts.splice(index, 1)
+  res.send(newData)
+})
+
+// GET /post/:postId/comments - Returns all comments associated with a post
+
+
+// get posts by postID
+app.get("/posts/:postID/comments", async (req, res) => {
+  const data = await fetch(basket)
+  const newData = await data.json()
+
+  //finds the specific postID then shares the post
+  const getId = Number(req.params.postID)
+
+  // return an array of the post (it should only return one specific item)
+  let id = newData.posts.filter((post) => post.postId === getId)
+  const  [ item ] = id
+  res.send(item.comments)
+})
