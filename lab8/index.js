a19abac116fb06192d8c402341086df59943c6af
Jaden Lewis-Chambers <you@example.com>
2023-03-24 00:58:45 -0400

    doing some things

--- lab8/index.js
+++ lab8/index.js
@@ -17,9 +17,34 @@ app.listen(PORT, () => {
 
 // Add your routes below:
 
-
+// get all posts
 app.get("/posts", async (req, res) => {
   const data = await fetch(basket)
   const newData = await data.json()
   res.send(newData)
+
+  console.log(newData.posts)
 })
+
+// get posts by author
+app.get("/posts/author/:author", async (req, res) => {
+  const author = req.params.author
+  const data = await fetch(basket)
+  const newData = await data.json()
+
+  const filteredPosts = newData.posts.filter((posts) => posts.author === author)
+
+  res.send(filteredPosts)
+})
+// get posts by postID
+app.get("/posts/author/:postID", async (req, res) => {
+  const author = req.params.postID
+  const data = await fetch(basket)
+  const newData = await data.json()
+
+  const filteredPosts = newData.posts.filter((posts) => posts.author === postID)
+
+  res.send(filteredPosts)
+})
+
+// post a post
